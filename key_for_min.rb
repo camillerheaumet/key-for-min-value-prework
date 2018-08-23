dafacce1a40df1664a0a0edb065a23e4a685185c
Camille Rheaume-Tremblay <camillerheaumet@hotmail.com>
2018-08-23 16:38:42 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,14 +4,10 @@
 def key_for_min_value(name_hash)
   loestKey = nil
   loestValue = nil
-  
-  #if name_hash.length > 0
-    name_hash.each do |key, value|
-       #key[value] <=> key[value + 1]
-       if loestValue == nil || value < loestValue
+  name_hash.each do |key, value|
+      if loestValue == nil || value < loestValue
          loestValue = value
          loestKey = key
-        
       end
   end
   loestKey
