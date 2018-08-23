a0f12d3c61fdf88b90aacc0fc42d7702ad870184
Camille Rheaume-Tremblay <camillerheaumet@hotmail.com>
2018-08-23 16:32:44 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   loestKey = nil
   loestValue = nil
   
-  if name_hash.length > 0
+  #if name_hash.length > 0
     name_hash.each do |key, value|
        #key[value] <=> key[value + 1]
        if value < loestValue
